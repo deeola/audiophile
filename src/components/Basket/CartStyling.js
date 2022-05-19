39dff0b756cbbdff2673b5c797836db50accebf8
Adeola Bamigboye <clazikar@gmail.com>
2022-05-19 13:31:26 +0200

    basket mobile and tablet styling

--- src/components/Basket/CartStyling.js
+++ src/components/Basket/CartStyling.js
@@ -8,7 +8,16 @@ padding: 20px;
 border-radius: 5px;
 top: 1.3%;
 left: 65%;
-margin-top: 50px;
+margin-top: 30px;
 position: absolute;
-// display: none;
+@media (max-width: 802px) {
+    width: 45%;
+    left: 45%;
+    margin-top: 30px;
+  }
+
+  @media (max-width: 490px) {
+    width: 80%;
+    left: 10%
+  }
 `
